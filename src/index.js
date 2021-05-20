76f33c055349e2fbae319a8263c5793c86cbabe4
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-05-20 20:30:30 -0300

    se agregaron middlewares y el index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 
+//setup
+//instancia de express
 const app = express();
 
-const port = 4000;
+//creamos el puerto
+app.set('port', process.env.PORT || 4000);
 
-app.listen(port, () => {
-    console.log('Estoy usando el puerto '+ port)
+app.listen(app.get('port'), () => {
+    console.log('estoy usando el puerto ' + app.get('port'));
+    
+});
+
+//middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '../public')));
+
+//rutas
+app.get('/', (req, res) => {
+    res.send('Hola desde el servidor');
 });
