995a778539d206bd37d33dae3a7431855f6e218d
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-05-24 20:25:31 -0300

    login encriptado funcionando

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -1,34 +1,36 @@
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+
+//emailhash $2a$08$YOQoHQrnsllKsjHVW.1w8uCOM6FFBLtvX22h.ODG.l33SXHS5Hw9O
+//passwordhash $2a$08$PPhD8VsCGvjT70noCYkyseWE1Jdg3ct5EClaTXfPtxhtAcRXe9WZW
 
 const adminCtrl = {};
 
 adminCtrl.getAdmin = async (req, res) => {
-    //datos a cargar a postman
-    const email = req.body.email;
-    const password = req.body.password;
-    //comprobamos que los datos de administrador son correctos
-    if(email === 'aaa@gmail.com' && password === '123'){
-        let passwordHash = await bcryptjs.hash(password, 8);
-        res.json({
-            message: "Autentificación exitosa!",
-            passwordHash: passwordHash
-        });
-    }else{
-        res.json({
-            message: "datos inválidos"
-        });
-    };
+  //datos a cargar a postman
+  const email = req.body.email;
+  const password = req.body.password;
+  //comprobamos que los datos de administrador son correctos
+  const passHash = "$2a$08$PPhD8VsCGvjT70noCYkyseWE1Jdg3ct5EClaTXfPtxhtAcRXe9WZW";
+  const adminHash = "$2a$08$YOQoHQrnsllKsjHVW.1w8uCOM6FFBLtvX22h.ODG.l33SXHS5Hw9O";
+  let comparePass = await bcryptjs.compareSync(password, passHash);
+  let compareAdmin = await bcryptjs.compareSync(email, adminHash);
+
+  if (compareAdmin && comparePass) {
+    res.json({
+      message: "Autentificación exitosa!",
+    });
+  } else {
+    res.json({
+      message:
+        "Datos inválidos, revise el usuario y contraseña de administrador.",
+    });
+  }
 };
 
 adminCtrl.getPrueba = (req, res) => {
-    //res.send('Hola desde el servidor');
-    let hashSaved = '$2a$08$PPhD8VsCGvjT70noCYkyseWE1Jdg3ct5EClaTXfPtxhtAcRXe9WZW'
-    let compare = bcryptjs.compareSync('123', hashSaved);
-    if(compare){
-        res.json('Ingreso exitoso!');
-    }else{
-        res.json('los datos ingresados no son correctos');
-    }
-    };
+  res.json({
+    message: "Hola desde el servidor y controlador del admin",
+  });
+};
 
 export default adminCtrl;
