2a9b123de24e34ea6c8639e398a6667c8dfd2b83
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-05-27 19:31:59 -0300

    modelo de noticia y rutas en rama correspondiente

--- src/routes/noticias.routes.js
+++ src/routes/noticias.routes.js
@@ -1,9 +1,16 @@
-import {Router} from 'express';
-import adminCtrl from '../controllers/admin.controllers'
+import { Router } from "express";
+import noticiaCtrl from "../controllers/noticia.controllers";
+
 
 const router = Router();
 
 //rutas
-router.route('/login').get(adminCtrl.getPrueba).post(adminCtrl.getAdmin);
+router.route("/").get(noticiaCtrl.getPrueba).post(noticiaCtrl.noticiaNueva);
+
+router
+  .route("/:_id")
+  .get(noticiaCtrl.obtenerNoticia)
+  .put(noticiaCtrl.editarNoticia)
+  .delete(noticiaCtrl.eliminarNoticia);
 
 export default router;
